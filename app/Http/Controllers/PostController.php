4a22269c8f01999016185ce1e81d33b67c7ee460
4r1es <GitHubkyusanschoolid@gmail.comgit config --global core.editor 'vim -c set>
2024-01-25 11:56:28 +0900

    honbun

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Post一覧を表示する
+     * 
+     * @param Post Postモデル
+     * @return array Postモデルリスト
+     */
+    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);  
+    }
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post'=>$post]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+}
