120836a670496e261f1ca28d500309cb6b4c3635
4r1es <GitHubkyusanschoolid@gmail.comgit config --global core.editor 'vim -c set>
2024-02-01 21:08:18 +0900

    token9.4

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,34 +1,26 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <h1>Blog Name</h1>
-        <a href="/posts/create">create</a>
-        
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <a href="/posts/{{$post->id}}"><h2 class='title'>{{ $post->title }}</h2></a>
-                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-                    <p class='body'>{{ $post->body }}</p>
-                    
-                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
-                    </form>
-                </div>
-            @endforeach
-        </div>
-        
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
+<x-app-layout>
+    <h1>Blog Name</h1>
+    <a href="/posts/create">create</a>
+    
+    <div class='posts'>
+        @foreach ($posts as $post)
+            <div class='post'>
+                <a href="/posts/{{$post->id}}"><h2 class='title'>{{ $post->title }}</h2></a>
+                <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                <p class='body'>{{ $post->body }}</p>
+                
+                <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+                </form>
+            </div>
+        @endforeach
+    </div>
+    
+    <div class='paginate'>
+        {{ $posts->links() }}
+    </div>
     <script>
         function deletePost(id) {
         'use strict'
@@ -39,6 +31,13 @@
     }
     </script>
     {{ Auth::user()->name }}
-    </body>
-   
-</html>
+    <div>
+        @foreach($questions as $question)
+            <div>
+                <a href="https://teratail.com/questions/{{ $question['id'] }}">
+                    {{ $question['title'] }}
+                </a>
+            </div>
+        @endforeach
+    </div>
+</x-app-layout>
